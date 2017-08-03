075dce8a4545ef0ede3e7ef76acc841b2cbb9a41
house11 <francesco.mizzi@libero.it>
2017-08-03 19:23:21 +0200

    Update and rename README.md to Luna.java

---
+++ Luna.java
@@ -0,0 +1,2 @@
+//che la gravità sulla luna è il 17% di quello della terra, scrivere un programma che calcoli il peso effettivo sulla luna di una //persona di 75 kg.
+//esempio una persona che sulla terra pesa 100kg sulla luna ne pesa 17kg
