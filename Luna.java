90062dd23a30083ebd39c6c29f3dd0a44b8b8e93
house11 <francesco.mizzi@libero.it>
2017-08-03 19:29:48 +0200

    Update Luna.java

--- Luna.java
+++ Luna.java
@@ -1,2 +1,17 @@
 //che la gravità sulla luna è il 17% di quello della terra, scrivere un programma che calcoli il peso effettivo sulla luna di una //persona di 75 kg.
 //esempio una persona che sulla terra pesa 100kg sulla luna ne pesa 17kg
+
+class Luna {
+
+
+  
+  public static void main(String args[]){
+    double pesoTerra = 75,pesoLuna=0;
+  
+    pesoLuna= pesoTerra (75*17)/100;
+    
+    System.println("Una persona che sulla terra pesa kg"+ pesoTerra+" sulla luna pesa "+ pesoLuna);
+  
+  }
+  
+}
