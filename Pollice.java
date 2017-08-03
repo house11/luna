54628226dbb27b68c038c888d8390fc5e35a18c9
house11 <francesco.mizzi@libero.it>
2017-08-03 19:52:57 +0200

    Create Pollice.java

---
+++ Pollice.java
@@ -0,0 +1,20 @@
+//Visualizzare, pollice per pollice,  una lista di conversione da pollici a metri fino a 100 pollici. Inserire una riga vuota ogni 10 pollici. 
+//Considerate che un metro equivale a 39,37 pollici.
+
+class Pollice{
+
+	
+	public static void main(String[] args) {
+	double pollici = 39.37;
+	int a=10;
+		
+
+		for(int i =0 ; i<101 ; i++){
+
+			System.out.println( i + " m    equivale a : "+(pollici*i));
+			if(i==a){	a=a+10;	System.out.println(); }
+
+		}
+		
+	}
+}
